8fe23147af263939f2cf626c668632a215fca944
EeUuGgEeNnEe-Nn <Eugene-log@bk.ru>
2023-05-03 19:23:48 +0300

    sv3

--- Bra2.py
+++ Bra2.py
@@ -2,3 +2,8 @@ data_list = [3, 5, 13, 4, 9, 20, 100]
 data_list = sorted(data_list)
 print(data_list)
 #print([i for i in data_list if i%2==0])
+three_dl = []
+for j in data_list:
+    if j%3==0:
+        three_dl.append(j)
+print(three_dl)
