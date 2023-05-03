e73c493b6641854c41e526a55303c7cd8a9cbec6
EeUuGgEeNnEe-Nn <Eugene-log@bk.ru>
2023-05-03 19:02:44 +0300

    sv2

--- Bra.py
+++ Bra.py
@@ -1,2 +1,4 @@
 data_list = [3, 5, 13, 4, 9, 20, 100]
 print([i for i in data_list if i%2==0])
+new_dl = sorted([i for i in data_list if i%2==0 or i%3==0], reverse=True)
+print(new_dl)
---
+++ Bra2.py
@@ -0,0 +1,4 @@
+data_list = [3, 5, 13, 4, 9, 20, 100]
+data_list = sorted(data_list)
+print(data_list)
+#print([i for i in data_list if i%2==0])
